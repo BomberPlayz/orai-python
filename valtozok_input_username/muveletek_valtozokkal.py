6958e97ecd25eb589681284ab1bfc9769fb9ee15
Farkas Richárd <49914603+BomberPlayz@users.noreply.github.com>
2024-09-18 08:58:37 +0000

    muveletek_valtozokkal placeholder helyett argumentumok

--- valtozok_input_username/muveletek_valtozokkal.py
+++ valtozok_input_username/muveletek_valtozokkal.py
@@ -1,8 +1,8 @@
 x=7
 y=3
 
-print("A {x} és az {y} összege: ", x+y)
-print("A {x} és az {y} különbsége: ", x-y)
-print("A {x} és az {y} szorzata: ", x*y)
-print("A {x} és az {y} háynadosa: ", x/y)
-print("A {x} és az {y} maradéka: ", x%y)
+print("A", x, "és", y, "összege:",x+y)
+print("A", x, "és", y, "különbsége:",x-y)
+print("A", x, "és", y, "szorzata:",x*y)
+print("A", x, "és", y, "hányadosa:",x/y)
+print("A", x, "és", y, "maradéka:",x%y)
